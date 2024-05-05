a2fb34858ae84cd435be8954c1698ae68c7bd0c6
Daniel Pagan <deapagan@gmail.com>
2024-05-04 17:26:04 -0700

    send m0m1

--- ChunkStream.fs
+++ ChunkStream.fs
@@ -1,24 +1,26 @@
 module rtmpoop.ChunkStream
 
-type Version = Version of uint8
+open System.Net.Sockets
 
-let rtmpoopVersion = Version 3uy
+type Version = uint8
+
+let rtmpoopVersion = 3uy
 
 
 let createVersion (b: uint8) =
     match b with
     | x when x <= 2uy -> Error "Deprecated version"
     | x when x >= 32uy -> Error "Version not allowed to disambiguate from text-based protocols"
-    | x -> Ok(Version x)
+    | x -> Ok(x)
 
 
 type Message0 = { version: Version }
 
-let createMessage0 = { version = rtmpoopVersion }
+let createMessage0 () = { version = rtmpoopVersion }
 
-type Time = Time of uint32
+type Time = uint32
 
-let epochStart = Time 0u
+let epochStart = 0u
 
 type Message1 =
     {
@@ -29,7 +31,7 @@ type Message1 =
 
 let randomDataLen = 1528
 
-let createMessage1 =
+let createMessage1 () =
     let rand = System.Random() in
 
     { time = epochStart
@@ -55,24 +57,85 @@ type HandshakeState =
     | AckSent
     | Done
 
+type AppState =
+    { handshake_state: HandshakeState
+      socket: Socket }
+      
+let createAppState sock = {
+    handshake_state=Uninitialized
+    socket=sock
+}
+
 type Message =
     | M0 of Message0
     | M1 of Message1
     | M2 of Message2
+    
+let send (socket: Socket) (b: byte array) =
+    let mutable sent = 0
+    let len = b.Length
+    while sent < len do
+        let tosend = b[sent..len-1]
+        let n = socket.Send tosend
+        sent <- sent + n
 
-let sendM0M1 () = Ok()
 
-let receiveM0 (msg: Message0) =
+let serializeM0 (m: Message0) =
+    [|byte m.version|]
+    
+let serializeM1 (m: Message1) =
+    System.BitConverter.GetBytes m.time
+    |> Array.append m.random_data
+
+let sendM0 (socket: Socket) =
+    let m0 = createMessage0 ()
+    send socket (serializeM0 m0)
+let sendM1 (socket: Socket) =
+    let m = createMessage1 ()
+    send socket (serializeM1 m)
+
+let receiveM0 (socket: Socket) (msg: Message0) =
     match msg.version with
-    | v when v <= rtmpoopVersion -> sendM0M1 ()
+    | v when v <= rtmpoopVersion -> 
+        socket
+        |> sendM0 
+
+        socket
+        |> sendM1
+        
+        Ok ()
     | _ -> Error "Unsupported version detected"
 
-let receive (state: HandshakeState) (msg: Message) =
-    match state with
+let recv (socket: Socket) =
+    let mutable buf = [||]
+    let mutable total = 0
+    let mutable keep_reading = true
+
+    while keep_reading do
+        let read_buf: byte array = [||]
+        let n = socket.Receive read_buf
+
+        if n = 0 then
+            keep_reading <- false
+        else
+            buf <- Array.append buf read_buf
+            total <- total + n
+
+    (buf, total)
+
+let parseMessage0 (msg_bytes: byte array, total: int) = 
+    if total <= 1 then Error "Not enough bytes for a parsed M0"
+    else 
+        createVersion msg_bytes[0] 
+        |> Result.bind (fun ver -> Ok {version = ver})
+
+let receive (state: AppState) =
+    match state.handshake_state with
     | Uninitialized ->
-        match msg with
-        | M0 m -> receiveM0 m
-        | _ -> Error "Uh oh stinky this is a bad message you sent me"
+        let msg_raw = recv state.socket
+        parseMessage0 msg_raw
+        |> Result.bind (receiveM0 state.socket)
+        |> Result.bind (fun _ -> Ok VersionSent)
     | VersionSent -> Error "not impl"
     | AckSent -> Error "not impl"
     | Done -> Error "not impl"
