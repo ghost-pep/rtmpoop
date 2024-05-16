40a633c5dce8f6f1dfe8fec3c368ffe060cc55ff
Daniel Pagan <danielpagan@microsoft.com>
2024-05-16 14:06:15 -0700

    receive m0 done

--- ChunkStream.fs
+++ ChunkStream.fs
@@ -24,10 +24,11 @@ let epochStart = 0u
 
 type Message1 =
     {
-      /// Time used as the epoch for all future timestamps
-      time: Time
-      /// Any random data of length 1528 bytes
-      random_data: uint8 array }
+        /// Time used as the epoch for all future timestamps
+        time: Time
+        /// Any random data of length 1528 bytes
+        random_data: uint8 array
+    }
 
 let randomDataLen = 1528
 
@@ -39,12 +40,13 @@ let createMessage1 () =
 
 type Message2 =
     {
-      /// Peer's timestamp received from Message1
-      time: Time
-      /// Local timestamp when Message1 was received
-      time2: Time
-      /// Random data field from Message1
-      random_echo: uint8 array }
+        /// Peer's timestamp received from Message1
+        time: Time
+        /// Local timestamp when Message1 was received
+        time2: Time
+        /// Random data field from Message1
+        random_echo: uint8 array
+    }
 
 let createMessage2 (cur: Time) (m1: Message1) =
     { time = m1.time
@@ -60,50 +62,53 @@ type HandshakeState =
 type AppState =
     { handshake_state: HandshakeState
       socket: Socket }
-      
-let createAppState sock = {
-    handshake_state=Uninitialized
-    socket=sock
-}
+
+let createSocket () =
+    let s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+    s.Bind(new System.Net.IPEndPoint(System.Net.IPAddress.Loopback, 1935))
+    s.Listen(10)
+    s
+
+let createAppState () =
+    { handshake_state = Uninitialized
+      socket = createSocket () }
 
 type Message =
     | M0 of Message0
     | M1 of Message1
     | M2 of Message2
-    
+
 let send (socket: Socket) (b: byte array) =
     let mutable sent = 0
     let len = b.Length
+
     while sent < len do
-        let tosend = b[sent..len-1]
+        let tosend = b[sent .. len - 1]
         let n = socket.Send tosend
         sent <- sent + n
 
 
-let serializeM0 (m: Message0) =
-    [|byte m.version|]
-    
+let serializeM0 (m: Message0) = [| byte m.version |]
+
 let serializeM1 (m: Message1) =
-    System.BitConverter.GetBytes m.time
-    |> Array.append m.random_data
+    System.BitConverter.GetBytes m.time |> Array.append m.random_data
 
 let sendM0 (socket: Socket) =
     let m0 = createMessage0 ()
     send socket (serializeM0 m0)
+
 let sendM1 (socket: Socket) =
     let m = createMessage1 ()
     send socket (serializeM1 m)
 
 let receiveM0 (socket: Socket) (msg: Message0) =
     match msg.version with
-    | v when v <= rtmpoopVersion -> 
-        socket
-        |> sendM0 
+    | v when v <= rtmpoopVersion ->
+        socket |> sendM0
 
-        socket
-        |> sendM1
-        
-        Ok ()
+        socket |> sendM1
+
+        Ok()
     | _ -> Error "Unsupported version detected"
 
 let recv (socket: Socket) =
@@ -112,7 +117,7 @@ let recv (socket: Socket) =
     let mutable keep_reading = true
 
     while keep_reading do
-        let read_buf: byte array = [||]
+        let read_buf: byte array = Array.zeroCreate 1024
         let n = socket.Receive read_buf
 
         if n = 0 then
@@ -123,19 +128,24 @@ let recv (socket: Socket) =
 
     (buf, total)
 
-let parseMessage0 (msg_bytes: byte array, total: int) = 
-    if total <= 1 then Error "Not enough bytes for a parsed M0"
-    else 
-        createVersion msg_bytes[0] 
-        |> Result.bind (fun ver -> Ok {version = ver})
+let parseMessage0 (msg_bytes: byte array, total: int) =
+    if total <= 1 then
+        Error "Not enough bytes for a parsed M0"
+    else
+        createVersion msg_bytes[0] |> Result.bind (fun ver -> Ok { version = ver })
 
 let receive (state: AppState) =
     match state.handshake_state with
     | Uninitialized ->
-        let msg_raw = recv state.socket
-        parseMessage0 msg_raw
-        |> Result.bind (receiveM0 state.socket)
-        |> Result.bind (fun _ -> Ok VersionSent)
+        let handler = state.socket.Accept()
+
+        recv handler
+        |> parseMessage0
+        |> Result.bind (receiveM0 handler)
+        |> Result.bind (fun _ ->
+            Ok
+                { state with
+                    handshake_state = VersionSent })
     | VersionSent -> Error "not impl"
     | AckSent -> Error "not impl"
     | Done -> Error "not impl"
--- Program.fs
+++ Program.fs
@@ -1,4 +1,11 @@
 ﻿open rtmpoop.ChunkStream
 
-let m0 = createMessage0
-printfn "%A" m0
+printfn "Starting up"
+let appstate = createAppState ()
+printfn "Created app state"
+let state2 = receive appstate
+printfn "Received state"
+
+match state2 with
+| Ok(s) -> printfn "Received state: %A" s
+| Error(e) -> printfn "Error: %s" e
